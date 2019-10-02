61f5b098ce513bcf69cbdd82a3afd200c1360429
Carlos Eduardo Simon Zepeda <casimon@redhat.com>
2019-10-02 18:57:40 -0500

    simple springboot kafka with String

---
+++ src/main/java/com/redhat/strema/controller/KafkaController.java
@@ -0,0 +1,24 @@
+package com.redhat.strema.controller;
+
+import com.redhat.strema.service.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kafka")
+public class KafkaController {
+  private final Producer producer;
+
+  @Autowired
+  KafkaController(Producer producer) {
+    this.producer = producer;
+  }
+
+  @PostMapping("/publish")
+  public void sendMessageTKafkaTopic(@RequestParam("message") String message) {
+    this.producer.sendMessage((message));
+  }
+}
